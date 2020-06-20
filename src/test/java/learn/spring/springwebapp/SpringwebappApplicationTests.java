57b45acd06a3d1187ab57c83919e25e6b4ce1bf2
Julian Schubert <juliankonstantinmaxschubert@outlook.com>
2020-06-20 13:52:26 +0200

    test commit

---
+++ src/test/java/learn/spring/springwebapp/SpringwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.spring.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebappApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
